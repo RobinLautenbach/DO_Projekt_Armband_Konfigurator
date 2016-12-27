6713476ecd021c8bccb3ef9415d73c1ed7902571
RobinLautenbach <robin.lautenbach@hotmail.de>
2016-12-27 22:19:47 +0100

    Änderungen Paginierung

--- Configurator/js/configurator.js
+++ Configurator/js/configurator.js
@@ -1,6 +1,7 @@
 var bConfigurator = (function($){
 	var bc = {};
 	var baseURI = 'http://localhost:8080/BraceletConfiguratorRestfulService/resources/bracelets';
+	var pagination_set = false;
 	bc.getBraceletsPaginated = function(start, size){
 			if(start >= 0 && size >= 0){
 				$.ajax({
@@ -19,7 +20,6 @@ var bConfigurator = (function($){
 							setNoDataDiv();
 						}else
 						{
-							console.log("Bracelets in DB:" + resp.getResponseHeader('braceletsInDB'));
 							listBracelets(resp.responseXML, resp.getResponseHeader('braceletsInDB'));
 						}
 					}
@@ -56,27 +56,38 @@ var bConfigurator = (function($){
 	bc.deleteBracelet = function(id){
 		console.log('Delete Bracelet with ID: ' + id);
 	};
-	function addPagination(items, itemsInDB){
+	bc.addPagination = function(items, itemsInDB){
 		var show_per_page = 10;
 		var number_of_pages = Math.ceil(itemsInDB/show_per_page);
 		var current_page = 1;
 		var html = '<ul>';
-		html += '<li><a href="javascript:prev();" id="prev_link">&lt;&lt;</a></li>';
+		html += '<li><a href="javascript:bConfigurator.prev();" id="prev_link">&lt;&lt;</a></li>';
 		for(var i=1;i<=number_of_pages;i++){
-			html += '<li><a href="javascript:goToPage(' + i + ');">' + i + '</a></li>';
+			html += '<li><a class="page_link" href="javascript:bConfigurator.goToPage(' + i + ');" longdesc="' + i + '">' + i + '</a></li>';
 		}
-		html += '<li><a href="javascript:next();" id="next_link">&gt;&gt;</a></li>';
-		html += '</ul>';
-		document.getElementById('pagination-container').innerHTML = html;
-		function prev(){
-			
-		}
-		function next(){
-			
-		}
-		function goToPage(page){
-			
-		}
+		html += '<li><a href="javascript:bConfigurator.next();" id="next_link">&gt;&gt;</a></li>';
+		html += '</ul><input type="hidden" id="current_page" /><input type="hidden" id="show_per_page" />';
+		$('#pagination-container').html(html);
+		$('#current_page').val(current_page);
+		$('#show_per_page').val(show_per_page);
+		$('#pagination-container .page_link:first').addClass('active_page');
+		pagination_set = true;
+	}
+	bc.prev = function(){
+		var new_page = parseInt($('#current_page').val()) - 1;
+		if($('.active_page').parent('li').prev('li').length == true) bc.goToPage(new_page);
+	}
+	bc.next = function(){	
+		var new_page = parseInt($('#current_page').val()) + 1;
+		if($('.active_page').parent('li').next('li').length == true) bc.goToPage(new_page);
+	}
+	bc.goToPage = function(page){
+		var show_per_page = parseInt($('#show_per_page').val());
+		var start = (page == 1) ? 0 : (page - 1) * show_per_page;
+		$('#pagination-container ul li a.active_page').removeClass('active_page');
+		$('.page_link[longdesc=' + page + ']').addClass('active_page');
+		$('#current_page').val(page);
+		bc.getBraceletsPaginated(start, show_per_page);
 	}
 	function template(item){
 		var html = '<tr>';
@@ -85,14 +96,14 @@ var bConfigurator = (function($){
 				html += '<td>' + $(item).attr('size') + '</td>';
 				html += '<td>' + $(item).attr('model') + '</td>';
 				html += '<td>' + $(item).attr('created') + '</td>';
-				html += '<td><a href="">Edit</a> | <a href="">Delete</a></td>';
+				html += '<td><a href="javascript:alert(\'edit\');">Edit</a> | <a href="javascript:alert(\'delete\');">Delete</a></td>';
 			html += '</tr>';
 		return html;
 	}
 	function listBracelets(xml, braceletsInDB){
 		var $xml = $(xml);
 		var bracelets = $xml.find('bracelet');
-		addPagination(bracelets, braceletsInDB);
+		if(!pagination_set) bc.addPagination(bracelets, braceletsInDB);
 		var html = '<table>';
 				html += '<tr>';
 					html += '<th>#ID</th>';
