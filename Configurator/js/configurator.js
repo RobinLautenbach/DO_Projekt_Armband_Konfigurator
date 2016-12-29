c8a84cfb441d47cdcf51ff37c4441f47beea49de
RobinLautenbach <robin.lautenbach@hotmail.de>
2016-12-29 11:12:04 +0100

    Bla

--- Configurator/js/configurator.js
+++ Configurator/js/configurator.js
@@ -74,34 +74,34 @@ var bConfigurator = (function($){
 	bc.addPagination = function(items, itemsInDB){
 		var show_per_page = 10;
 		var number_of_pages = Math.ceil(itemsInDB/show_per_page);
-		var current_page = 1;
+		var current_page;
+		$('#current_page').length == 0 ? current_page = 1 : current_page = parseInt($('#current_page').val());
 		var html = '<ul>';
 		html += '<li><a href="javascript:bConfigurator.prev();" id="prev_link">&lt;&lt;</a></li>';
 		for(var i=1;i<=number_of_pages;i++){
-			html += '<li class="pagination_page_li"><a class="page_link" href="javascript:bConfigurator.goToPage(' + i + ');" longdesc="' + i + '">' + i + '</a></li>';
+			html += '<li class="pagination_page_li"><a href="javascript:bConfigurator.goToPage(' + i + ');" longdesc="' + i + '">' + i + '</a></li>';
 		}
 		html += '<li><a href="javascript:bConfigurator.next();" id="next_link">&gt;&gt;</a></li>';
 		html += '</ul><input type="hidden" id="current_page" /><input type="hidden" id="show_per_page" />';
 		$('#pagination-container').html(html);
 		$('#current_page').val(current_page);
 		$('#show_per_page').val(show_per_page);
-		$('#page_count').val(number_of_pages);
-		$('#pagination-container .page_link:first').addClass('active_page');
+		current_page == 1 ? $('#pagination-container .pagination_page_li:first').addClass('active_page') : $('#pagination-container li:nth-child(' + (current_page + 1) + ')').addClass('active_page');		
 		pagination_set = true;
 	}
 	bc.prev = function(){
 		var new_page = parseInt($('#current_page').val()) - 1;
-		if($('.active_page').parent('li').prev('li.pagination_page_li').length == true) bc.goToPage(new_page);
+		if($('.active_page').prev('li.pagination_page_li').length == true) bc.goToPage(new_page);
 	}
 	bc.next = function(){	
 		var new_page = parseInt($('#current_page').val()) + 1;
-		if($('.active_page').parent('li').next('li.pagination_page_li').length == true) bc.goToPage(new_page);
+		if($('.active_page').next('li.pagination_page_li').length == true) bc.goToPage(new_page);
 	}
 	bc.goToPage = function(page){
 		var show_per_page = parseInt($('#show_per_page').val());
 		var start = (page == 1) ? 0 : (page - 1) * show_per_page;
-		$('#pagination-container ul li a.active_page').removeClass('active_page');
-		$('.page_link[longdesc=' + page + ']').addClass('active_page');
+		$('#pagination-container ul li.active_page').removeClass('active_page');
+		$('.pagination_page_li a[longdesc=' + page + ']').parent('li').addClass('active_page');
 		$('#current_page').val(page);
 		bc.getBraceletsPaginated(start, show_per_page);
 	}
